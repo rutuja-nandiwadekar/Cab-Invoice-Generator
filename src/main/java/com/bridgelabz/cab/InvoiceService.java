000e6acd8b80ce2fccdb4f44e4a6a5bcad3bea03
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-24 21:01:27 +0530

    Refactored: Calculate minimum fare logic

--- src/main/java/com/bridgelabz/cab/InvoiceService.java
+++ src/main/java/com/bridgelabz/cab/InvoiceService.java
@@ -3,8 +3,13 @@ package com.bridgelabz.cab;
 public class InvoiceService {
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final int MINIMUM_COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance* MINIMUM_COST_PER_KM + time * MINIMUM_COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * MINIMUM_COST_PER_TIME;
+
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
